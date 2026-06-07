e8c2c51da76bbd70ae124523b1cb49d92e6304f1
GLBITM-25110CN351 <dipendralama38555@gmail.com>
2026-06-07 19:05:20 +0530

    java programs

---
+++ Multiplication_Table.java
@@ -0,0 +1,42 @@
+//enter number = 6;
+// 6 * 1 = 6;
+// 6 * 2 = 12;
+import java.util.Scanner;
+class Multiplication_Table{
+    public static void main (String[]args){
+        int num;
+        System.out.print("enter any number ");
+        Scanner r = new Scanner(System.in);
+       num =r. nextInt();
+       for(int i  = 1; i <= 10; i++){
+         System.out.println(num +" * " +" = " +(num * i));
+       }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
